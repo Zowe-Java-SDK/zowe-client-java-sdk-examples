50be3d4faadea15ce9a3a2583a37c8557a5e4570
Frank Giordano <giofr01@yahoo.com>
2022-08-24 15:51:26 -0400

    added new secure credential retrieval example

--- src/main/java/zowe/client/sdk/examples/ZosConnection.java
+++ src/main/java/zowe/client/sdk/examples/ZosConnection.java
@@ -9,6 +9,13 @@
  */
 package zowe.client.sdk.examples;
 
+import zowe.client.sdk.core.ZOSConnection;
+import zowe.client.sdk.teamconfig.TeamConfig;
+import zowe.client.sdk.teamconfig.keytar.KeyTarImpl;
+import zowe.client.sdk.teamconfig.model.ProfileDao;
+import zowe.client.sdk.teamconfig.service.KeyTarService;
+import zowe.client.sdk.teamconfig.service.TeamConfigService;
+
 /**
  * Base class with connection member static variables for use by examples to provide a means of a shortcut to avoid
  * duplicating connection details in each example.
@@ -18,9 +25,19 @@ package zowe.client.sdk.examples;
  */
 public class ZosConnection {
 
+    // replace "XXX" with hard coded values to execute the examples in this project
     public static final String hostName = "XXX";
     public static final String zosmfPort = "XXX";
     public static final String userName = "XXX";
     public static final String password = "XXX";
+
+    // or use the following method to retrieve Zowe OS credential store for your
+    // secure Zowe V2 credentials you entered when you initially setup Zowe Global
+    // Team Configuration.
+    public static zowe.client.sdk.core.ZOSConnection getSecureZosConnection() throws Exception {
+        TeamConfig teamConfig = new TeamConfig(new KeyTarService(new KeyTarImpl()), new TeamConfigService());
+        ProfileDao profile = teamConfig.getDefaultProfileByName("zosmf");
+        return (new ZOSConnection(profile.getHost(), profile.getPort(), profile.getUser(), profile.getPassword()));
+    }
 
 }
