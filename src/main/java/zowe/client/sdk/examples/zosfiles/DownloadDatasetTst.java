24b7d777ad8aeaa2f76bee305740f1f330acc85e
Frank Giordano <giofr01@yahoo.com>
2023-06-12 16:55:28 -0400

    add sequential download example

--- src/main/java/zowe/client/sdk/examples/zosfiles/DownloadDatasetTst.java
+++ src/main/java/zowe/client/sdk/examples/zosfiles/DownloadDatasetTst.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 import java.io.StringWriter;
 
 /**
- * Class example to showcase DownloadDataset functionality.
+ * Class example to showcase DownloadDataset via DsnGet class.
  *
  * @author Leonid Baranov
  * @author Frank Giordano
@@ -27,19 +27,21 @@ public class DownloadDatasetTst extends TstZosConnection {
      * @author Leonid Baranov
      */
     public static void main(String[] args) throws Exception {
-        String datasetName = "xxx.xxx.xxx";
+        String datasetName = "xxx";
+        String datasetSeqName = "xxx";
         String memberName = "xxx";
         DownloadParams params = new DownloadParams.Builder().build();
         ZOSConnection connection = new ZOSConnection(hostName, zosmfPort, userName, password);
         DownloadDatasetTst.downloadDsnMember(connection, datasetName, memberName, params);
+        DownloadDatasetTst.downloadDsnSequential(connection, datasetSeqName, params);
     }
 
     /**
      * Download a dataset member
      *
      * @param connection ZOSConnection object
-     * @param dsName     data set name
-     * @param memName    member name that exists within the specified data set name
+     * @param dsName     name of a dataset
+     * @param memName    member name that exists within the specified dataset name
      * @param params     download parameters object
      * @throws Exception error processing request
      * @author Leonid Baranov
@@ -48,6 +50,27 @@ public class DownloadDatasetTst extends TstZosConnection {
                                          DownloadParams params) throws Exception {
         try (InputStream inputStream = new DsnGet(connection)
                 .get(String.format("%s(%s)", dsName, memName), params)) {
+            if (inputStream != null) {
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(inputStream, writer, "UTF8");
+                String content = writer.toString();
+                System.out.println(content);
+            }
+        }
+    }
+
+    /**
+     * Download a sequential dataset
+     *
+     * @param connection ZOSConnection object
+     * @param dsName     name of a sequential dataset
+     * @param params     download parameters object
+     * @throws Exception error processing request
+     * @author Frank Giordano
+     */
+    public static void downloadDsnSequential(ZOSConnection connection, String dsName, DownloadParams params)
+            throws Exception {
+        try (InputStream inputStream = new DsnGet(connection).get(dsName, params)) {
             if (inputStream != null) {
                 StringWriter writer = new StringWriter();
                 IOUtils.copy(inputStream, writer, "UTF8");
