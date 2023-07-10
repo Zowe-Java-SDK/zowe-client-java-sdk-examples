2493c5a3ecf21f36a80c9affb267382954a9985e
Frank Giordano <giofr01@yahoo.com>
2023-07-10 14:55:05 -0400

    Update SubmitJobTst.java

--- src/main/java/zowe/client/sdk/examples/zosjobs/SubmitJobTst.java
+++ src/main/java/zowe/client/sdk/examples/zosjobs/SubmitJobTst.java
@@ -1,6 +1,6 @@
 package zowe.client.sdk.examples.zosjobs;
 
-import zowe.client.sdk.core.ZOSConnection;
+import zowe.client.sdk.core.ZosConnection;
 import zowe.client.sdk.examples.TstZosConnection;
 import zowe.client.sdk.zosjobs.methods.JobMonitor;
 import zowe.client.sdk.zosjobs.methods.JobSubmit;
@@ -24,7 +24,7 @@ public class SubmitJobTst extends TstZosConnection {
      * @author Frank Giordano
      */
     public static void main(String[] args) throws Exception {
-        ZOSConnection connection = new ZOSConnection(hostName, zosmfPort, userName, password);
+        ZosConnection connection = new ZosConnection(hostName, zosmfPort, userName, password);
         System.out.println(SubmitJobTst.submitJob(connection, "xxx.xxx.xxx.xxx(xxx)"));
 
         String jclString = "//TESTJOBX JOB (),MSGCLASS=H\n// EXEC PGM=IEFBR14";
@@ -42,13 +42,13 @@ public class SubmitJobTst extends TstZosConnection {
      * Example on how to call JobSubmit submitByJcl method.
      * The submitByJcl method is given a jcl string to use to submit it as a job.
      *
-     * @param connection ZOSConnection object
+     * @param connection ZosConnection object
      * @param jclString  jcl formatted string
      * @return job document
      * @throws Exception error in processing request
      * @author Frank Giordano
      */
-    public static Job submitJclJob(ZOSConnection connection, String jclString) throws Exception {
+    public static Job submitJclJob(ZosConnection connection, String jclString) throws Exception {
         JobSubmit jobSubmit = new JobSubmit(connection);
         return jobSubmit.submitByJcl(jclString, null, null);
     }
@@ -57,13 +57,13 @@ public class SubmitJobTst extends TstZosConnection {
      * Example on how to call JobSubmit submit method.
      * The submit method is given a Dataset member value to use to submit it as a job.
      *
-     * @param connection ZOSConnection object
+     * @param connection ZosConnection object
      * @param dsMember   dataset member value
      * @return job document
      * @throws Exception error in processing request
      * @author Frank Giordano
      */
-    public static Job submitJob(ZOSConnection connection, String dsMember) throws Exception {
+    public static Job submitJob(ZosConnection connection, String dsMember) throws Exception {
         JobSubmit jobSubmit = new JobSubmit(connection);
         return jobSubmit.submit(dsMember);
     }
