5967490dd7d3e6cb321d7a58e497b863bafaea11
Frank Giordano <giofr01@yahoo.com>
2023-07-10 16:16:14 -0400

    Update CreateUssTst.java

--- src/main/java/zowe/client/sdk/examples/zosfiles/CreateUssTst.java
+++ src/main/java/zowe/client/sdk/examples/zosfiles/CreateUssTst.java
@@ -11,7 +11,7 @@ import zowe.client.sdk.zosfiles.uss.types.CreateType;
  * Class example to test unix system services create command functionality via UssCreate class.
  *
  * @author Frank Giordano
- * @version 1.0
+ * @version 2.0
  */
 public class CreateUssTst extends TstZosConnection {
 
